cf961295dc40053a33629edafbae19b719657409
Andrey Svetlichny <andrey.svetlichny@gmail.com>
2025-01-18 16:21:18 +0100

    tokyonight tuning; have_nerd_font = true

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -853,6 +853,8 @@ require('lazy').setup({
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
+      vim.cmd.hi 'Comment guifg=#7885bf'
+      vim.cmd.hi 'Normal guibg=#16161e'
     end,
   },
 
