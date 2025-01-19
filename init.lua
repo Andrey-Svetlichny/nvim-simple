144016105ecb583e69dfc20f66af4daacc1ec0ae
Andrey Svetlichny <andrey.svetlichny@gmail.com>
2025-01-19 13:22:02 +0100

    LSP servers

--- init.lua
+++ init.lua
@@ -634,9 +634,13 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        angularls = {},
         -- clangd = {},
-        -- gopls = {},
+        cssls = {},
+        html = {},
+        gopls = {},
         pyright = {},
+        ts_ls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
